840f03c39b93230718a405337a93758f2a0a7ef4
Yuji Sugiura <y.sugiura.0316@gmail.com>
2019-12-11 12:31:38 +0900

    Apply prettier

--- src/player.ts
+++ src/player.ts
@@ -2,165 +2,183 @@
 /// <reference path="ring_buffer.ts" />
 
 class WebAudioPlayer {
-    private context: AudioContext;
-    private node: ScriptProcessorNode;
-    private resampler: Worker;
-    private in_writing: boolean = false;
+  private context: AudioContext;
+  private node: ScriptProcessorNode;
+  private resampler: Worker;
+  private in_writing: boolean = false;
 
-    private buffering: boolean = true;
-    private ringbuf: RingBuffer;
-    private period_samples: number;
-    private delay_samples: number;
+  private buffering: boolean = true;
+  private ringbuf: RingBuffer;
+  private period_samples: number;
+  private delay_samples: number;
 
-    onneedbuffer: () => void = null;
+  onneedbuffer: () => void = null;
 
-    init(sampling_rate: number, num_of_channels: number,
-         period_samples: number, delay_periods: number, buffer_periods: number): Promise<any>
-    {
-        return new Promise((resolve, reject) => {
-            this.context = new AudioContext();
-            this.node = this.context.createScriptProcessor(period_samples, 0, num_of_channels);
-            this.node.onaudioprocess = (ev) => {
-                this._onaudioprocess(ev);
-            };
-            if (sampling_rate != this.getActualSamplingRate()) {
-                console.log('enable resampling: ' + sampling_rate + ' -> ' + this.getActualSamplingRate());
-                this.period_samples = Math.ceil(period_samples * this.getActualSamplingRate() / sampling_rate) * num_of_channels;
-                this.resampler = new Worker('resampler.js');
-            } else {
-                this.period_samples = period_samples * num_of_channels;
-            }
-            this.ringbuf = new RingBuffer(new Float32Array(this.period_samples * buffer_periods));
-            this.delay_samples = this.period_samples * delay_periods;
-            if (this.resampler) {
-                this.resampler.onmessage = (ev) => {
-                    if (ev.data.status == 0) {
-                        resolve();
-                    } else {
-                        reject(ev.data);
-                    }
-                };
-                this.resampler.postMessage({
-                    channels: num_of_channels,
-                    in_sampling_rate: sampling_rate,
-                    out_sampling_rate: this.getActualSamplingRate()
-                });
-            } else {
-                resolve();
-            }
-        });
-    }
-
-    enqueue(buf: IAudioBuffer): Promise<any> {
-        return new Promise((resolve, reject) => {
-            if (this.in_writing) {
-                reject();
-                return;
-            }
-            this.in_writing = true;
-            var func = (data: Float32Array) => {
-                this.ringbuf.append(data).then(() => {
-                    this.in_writing = false;
-                    this.check_buffer();
-                }, (e) => {
-                    this.in_writing = false;
-                    reject(e);
-                });
-            };
-            if (this.resampler) {
-                var transfer_list = buf.transferable ? [buf.samples.buffer] : [];
-                this.resampler.onmessage = (ev) => {
-                    if (ev.data.status != 0) {
-                        this.in_writing = false;
-                        reject(ev.data);
-                        return;
-                    }
-                    func(ev.data.result);
-                };
-                this.resampler.postMessage({
-                    samples: buf.samples
-	            }, transfer_list);
-            } else {
-                func(buf.samples);
-            }
-        });
-    }
-
-    private _onaudioprocess(ev): void {
-        if (this.buffering) {
-            this.check_buffer();
-            return;
-        }
-        var N = ev.outputBuffer.numberOfChannels;
-        var buf = new Float32Array(ev.outputBuffer.getChannelData(0).length * N);
-        var size = this.ringbuf.read_some(buf) / N;
-        for (var i = 0; i < N; ++i) {
-            var ch = ev.outputBuffer.getChannelData(i);
-            for (var j = 0; j < size; ++j)
-                ch[j] = buf[j * N + i];
-        }
-        this.check_buffer(true);
-    }
-
-    private in_requesting_check_buffer = false;
-    private check_buffer(useTimeOut: boolean = false): void {
-        if (this.in_requesting_check_buffer || !this.onneedbuffer)
-            return;
-        var needbuf = this.check_buffer_internal();
-        if (!needbuf)
-            return;
-        if (useTimeOut) {
-            this.in_requesting_check_buffer = true;
-            window.setTimeout(() => {
-                this.in_requesting_check_buffer = false;
-                if (this.check_buffer_internal())
-                    this.onneedbuffer();
-            }, 0);
-        } else {
-            this.onneedbuffer();
-        }
-    }
-
-    private check_buffer_internal(): boolean {
-        if (this.in_writing) return false;
-        var avail = this.ringbuf.available();
-        var size = this.ringbuf.size();
-        if (size >= this.delay_samples)
-            this.buffering = false;
-        if (this.period_samples <= avail)
-            return true;
-        return false;
-    }
-
-    start(): void {
-        if (this.node) {
-            this.node.connect(this.context.destination);
-        }
-    }
-
-    stop(): void {
-        if (this.node) {
-            this.ringbuf.clear();
-            this.buffering = true;
-            this.node.disconnect();
-        }
-    }
-
-    close(): void {
-        this.stop();
-        this.context = null;
-        this.node = null;
-    }
-
-    getActualSamplingRate(): number {
-        return this.context.sampleRate;
-    }
-
-    getBufferStatus(): IPlayerBufferStatus {
-        return {
-            delay: this.ringbuf.size(),
-            available: this.ringbuf.available(),
-            capacity: this.ringbuf.capacity(),
+  init(
+    sampling_rate: number,
+    num_of_channels: number,
+    period_samples: number,
+    delay_periods: number,
+    buffer_periods: number
+  ): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.context = new AudioContext();
+      this.node = this.context.createScriptProcessor(
+        period_samples,
+        0,
+        num_of_channels
+      );
+      this.node.onaudioprocess = ev => {
+        this._onaudioprocess(ev);
+      };
+      if (sampling_rate != this.getActualSamplingRate()) {
+        console.log(
+          "enable resampling: " +
+            sampling_rate +
+            " -> " +
+            this.getActualSamplingRate()
+        );
+        this.period_samples =
+          Math.ceil(
+            (period_samples * this.getActualSamplingRate()) / sampling_rate
+          ) * num_of_channels;
+        this.resampler = new Worker("resampler.js");
+      } else {
+        this.period_samples = period_samples * num_of_channels;
+      }
+      this.ringbuf = new RingBuffer(
+        new Float32Array(this.period_samples * buffer_periods)
+      );
+      this.delay_samples = this.period_samples * delay_periods;
+      if (this.resampler) {
+        this.resampler.onmessage = ev => {
+          if (ev.data.status == 0) {
+            resolve();
+          } else {
+            reject(ev.data);
+          }
         };
+        this.resampler.postMessage({
+          channels: num_of_channels,
+          in_sampling_rate: sampling_rate,
+          out_sampling_rate: this.getActualSamplingRate()
+        });
+      } else {
+        resolve();
+      }
+    });
+  }
+
+  enqueue(buf: IAudioBuffer): Promise<any> {
+    return new Promise((resolve, reject) => {
+      if (this.in_writing) {
+        reject();
+        return;
+      }
+      this.in_writing = true;
+      var func = (data: Float32Array) => {
+        this.ringbuf.append(data).then(
+          () => {
+            this.in_writing = false;
+            this.check_buffer();
+          },
+          e => {
+            this.in_writing = false;
+            reject(e);
+          }
+        );
+      };
+      if (this.resampler) {
+        var transfer_list = buf.transferable ? [buf.samples.buffer] : [];
+        this.resampler.onmessage = ev => {
+          if (ev.data.status != 0) {
+            this.in_writing = false;
+            reject(ev.data);
+            return;
+          }
+          func(ev.data.result);
+        };
+        this.resampler.postMessage(
+          {
+            samples: buf.samples
+          },
+          transfer_list
+        );
+      } else {
+        func(buf.samples);
+      }
+    });
+  }
+
+  private _onaudioprocess(ev): void {
+    if (this.buffering) {
+      this.check_buffer();
+      return;
     }
+    var N = ev.outputBuffer.numberOfChannels;
+    var buf = new Float32Array(ev.outputBuffer.getChannelData(0).length * N);
+    var size = this.ringbuf.read_some(buf) / N;
+    for (var i = 0; i < N; ++i) {
+      var ch = ev.outputBuffer.getChannelData(i);
+      for (var j = 0; j < size; ++j) ch[j] = buf[j * N + i];
+    }
+    this.check_buffer(true);
+  }
+
+  private in_requesting_check_buffer = false;
+  private check_buffer(useTimeOut: boolean = false): void {
+    if (this.in_requesting_check_buffer || !this.onneedbuffer) return;
+    var needbuf = this.check_buffer_internal();
+    if (!needbuf) return;
+    if (useTimeOut) {
+      this.in_requesting_check_buffer = true;
+      window.setTimeout(() => {
+        this.in_requesting_check_buffer = false;
+        if (this.check_buffer_internal()) this.onneedbuffer();
+      }, 0);
+    } else {
+      this.onneedbuffer();
+    }
+  }
+
+  private check_buffer_internal(): boolean {
+    if (this.in_writing) return false;
+    var avail = this.ringbuf.available();
+    var size = this.ringbuf.size();
+    if (size >= this.delay_samples) this.buffering = false;
+    if (this.period_samples <= avail) return true;
+    return false;
+  }
+
+  start(): void {
+    if (this.node) {
+      this.node.connect(this.context.destination);
+    }
+  }
+
+  stop(): void {
+    if (this.node) {
+      this.ringbuf.clear();
+      this.buffering = true;
+      this.node.disconnect();
+    }
+  }
+
+  close(): void {
+    this.stop();
+    this.context = null;
+    this.node = null;
+  }
+
+  getActualSamplingRate(): number {
+    return this.context.sampleRate;
+  }
+
+  getBufferStatus(): IPlayerBufferStatus {
+    return {
+      delay: this.ringbuf.size(),
+      available: this.ringbuf.available(),
+      capacity: this.ringbuf.capacity()
+    };
+  }
 }
--- src/resampler.ts
+++ src/resampler.ts
@@ -2,51 +2,54 @@
 /// <reference path="speex_resampler.ts" />
 
 class ResamplingWorker {
-    worker: Worker;
-    resampler: SpeexResampler = null;
+  worker: Worker;
+  resampler: SpeexResampler = null;
 
-    constructor(worker: Worker) {
-        this.worker = worker;
-        this.worker.onmessage = (e: MessageEvent) => {
-            this.setup(<any>e.data);
-        };
-    }
+  constructor(worker: Worker) {
+    this.worker = worker;
+    this.worker.onmessage = (e: MessageEvent) => {
+      this.setup(<any>e.data);
+    };
+  }
 
-    setup(config: any) {
-        try {
-            this.resampler = new SpeexResampler(
-                config.channels,
-                config.in_sampling_rate,
-                config.out_sampling_rate,
-                config.quality || 5
-            );
-            this.worker.postMessage({
-                status: 0
-            });
-            this.worker.onmessage = (e: MessageEvent) => {
-                this.process(<Float32Array>e.data.samples);
-            };
-        } catch (e) {
-            this.worker.postMessage({
-                status: -1,
-                reason: e
-            });
-        }
+  setup(config: any) {
+    try {
+      this.resampler = new SpeexResampler(
+        config.channels,
+        config.in_sampling_rate,
+        config.out_sampling_rate,
+        config.quality || 5
+      );
+      this.worker.postMessage({
+        status: 0
+      });
+      this.worker.onmessage = (e: MessageEvent) => {
+        this.process(<Float32Array>e.data.samples);
+      };
+    } catch (e) {
+      this.worker.postMessage({
+        status: -1,
+        reason: e
+      });
     }
+  }
 
-    process(input: Float32Array) {
-        try {
-            var ret = new Float32Array(this.resampler.process(input));
-            this.worker.postMessage({
-                status: 0,
-                result: ret,
-            }, [ret.buffer]);
-        } catch (e) {
-            this.worker.postMessage({
-                status: -1,
-                reason: e
-            });
-        }
+  process(input: Float32Array) {
+    try {
+      var ret = new Float32Array(this.resampler.process(input));
+      this.worker.postMessage(
+        {
+          status: 0,
+          result: ret
+        },
+        [ret.buffer]
+      );
+    } catch (e) {
+      this.worker.postMessage({
+        status: -1,
+        reason: e
+      });
     }
+  }
 }
 new ResamplingWorker(this);
